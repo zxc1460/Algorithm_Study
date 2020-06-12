68f3be37f18fde7cb30c1c6f7fd7e55d1a0a958d
zxc1460 <zxc1460@naver.com>
2020-06-12 09:46:15 +0900

    Update 석도형-lv1-K번째수.cpp

--- 2주차/석도형-lv1-K번째수.cpp
+++ 2주차/석도형-lv1-K번째수.cpp
@@ -4,23 +4,17 @@
 
 using namespace std;
 
-bool cmp(string a, string b) {
-	return a + b > b + a;
-}
-
-string solution(vector<int> numbers) {
-    string answer = "";
-    vector<string> v;
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
     
-    for(int num : numbers) {
-    	v.push_back(to_string(num));
+    for(int i = 0; i < commands.size(); i++) {
+    	vector<int> temp;
+    	for(int j = commands[i][0] - 1; j < commands[i][1]; j++) {
+    		temp.push_back(array[j]);
+		}
+		sort(temp.begin(), temp.end());
+		answer.push_back(temp[commands[i][2] - 1]);
 	}
-	
-	sort(v.begin(), v.end(), cmp);
-	
-	for(string s : v) answer += s;
-	
-	if(answer.front() == '0') answer = "0";
     
     return answer;
 }
